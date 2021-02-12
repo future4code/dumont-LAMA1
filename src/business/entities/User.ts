df645ed0d92d828ec441f4b13ccd67ac50b540cc
Tainah Boarini Bernardo <tainahboarini@gmail.com>
2021-02-12 11:13:24 -0300

    refatoração 3

--- src/business/entities/User.ts
+++ src/business/entities/User.ts
@@ -28,7 +28,7 @@ export class User {
 
 
   static stringToUserRole(input: string): UserRole {
-     switch (input) {
+     switch (input.toUpperCase()) {
         case "NORMAL":
            return UserRole.NORMAL;
         case "ADMIN":
